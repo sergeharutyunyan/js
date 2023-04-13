8ed72ea4c7dd295e3b63014c57330b4c287a06c5
Serge <shinfo92@gmail.com>
2023-04-13 23:11:09 +0400

    type 6 primitive 1 link

---
+++ 1/js/main.js
@@ -0,0 +1,33 @@
+// let a;
+// console.log(a)
+// a = "kuku";
+// console.log(a)
+// console.log(console.log(10))
+
+// const myName = "Serge";
+
+// myName = "";
+
+// console.log(myName);
+// typeof myName;
+
+// type 6 primitive 1 link
+/*
+const objectA = {
+    a: 10,
+    b: true
+}
+
+const copyOfA = objectA;
+console.log(objectA)
+console.log(copyOfA)
+
+copyOfA.a = 25;
+
+console.log(objectA)
+console.log(copyOfA)
+
+copyOfA.c = "Serge";
+
+console.log(objectA)
+console.log(copyOfA)*/
