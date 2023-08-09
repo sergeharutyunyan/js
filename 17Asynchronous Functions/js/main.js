7487fc87f74304735bab8ed9c232b9f585511a4d
Serge <shinfo92@gmail.com>
2023-08-09 11:30:38 +0400

    comleted lessons start parakick

--- 17Asynchronous Functions/js/main.js
+++ 17Asynchronous Functions/js/main.js
@@ -23,6 +23,53 @@ asyncFn()
     .then(value => console.log(value))
     .catch(error => console.log(error.message))*/
 
+/*
 const timerPromise = () =>
     new Promise((resolve, reject) =>
     setTimeout(()=> resolve(), 2000))
+
+const asyncFn = async () =>{
+    console.log("Timer start")
+    await timerPromise()
+    console.log('Timer endend')
+}
+
+asyncFn()
+*/
+
+/*
+const timerPromise = () =>
+    new Promise((resolve, reject) =>
+        setTimeout(()=> resolve(), 2000))
+
+const asyncFn = async () =>{
+    console.log("Timer start")
+    const startTime = performance.now()
+    console.log(startTime)
+    await timerPromise()
+    const endTime = performance.now()
+    console.log('Timer endend', endTime - startTime)
+}
+
+asyncFn()
+
+*/
+
+
+const getData = async (url) =>{
+    const res = await fetch(url)
+    const json = await res.json()
+    return json
+}
+
+
+
+const myUrl = 'https://jsonplaceholder.typicode.com/todos/'
+
+try {
+    const data = await getData(myUrl)
+    console.log(data)
+}catch (error){
+    console.log(error.message)
+}
+
